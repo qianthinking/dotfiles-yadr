96899b8a0a8ef1f9d05b7202111f8e98f8c7741a
Leon Li <qianthinking@gmail.com>
2023-03-01 16:51:33 +0800

    chore: default no nvim-tree

--- vim/settings/nvim-tree.lua
+++ vim/settings/nvim-tree.lua
@@ -84,6 +84,6 @@ local function open_nvim_tree()
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 vim.api.nvim_set_keymap("n", "tn", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<C-\\>", ":NvimTreeFindFile<CR>", {noremap = true, silent = true})
