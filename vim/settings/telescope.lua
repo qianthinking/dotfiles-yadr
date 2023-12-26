6437c9f0435d33a6a3fe554d8aad53eba70bc90e
Leon Li <qianthinking@gmail.com>
2023-12-26 14:21:15 +0800

    chore(vim): update file opening logic in telescope.lua

--- vim/settings/telescope.lua
+++ vim/settings/telescope.lua
@@ -2,23 +2,39 @@ local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 
 local function open_or_switch_to_file(prompt_bufnr)
-    local selected_file = action_state.get_selected_entry()
+    local selected_entry = action_state.get_selected_entry()
     actions.close(prompt_bufnr)
 
-    -- Iterate over all tabs and buffers
+    local file_to_open
+    local line_nr
+    if type(selected_entry.value) == "table" then
+        -- Handle complex entries (like from live_grep)
+        file_to_open = selected_entry.value.filename or selected_entry.value[1]
+        line_nr = selected_entry.value.lnum
+    else
+        -- Handle simple file paths
+        file_to_open = selected_entry.value
+    end
+
+    -- Check if the file is already open in a tab
     for tab = 1, vim.fn.tabpagenr('$') do
         for _, bufnr in ipairs(vim.fn.tabpagebuflist(tab)) do
-            if vim.fn.bufname(bufnr) == selected_file.value then
-                -- Switch to the tab and buffer if the file is already open
+            if vim.fn.bufname(bufnr) == file_to_open then
                 vim.cmd(tab .. 'tabnext')
                 vim.cmd('buffer ' .. bufnr)
+                if line_nr then
+                    vim.api.nvim_win_set_cursor(0, {line_nr, 0})
+                end
                 return
             end
         end
     end
 
     -- Open the file in the current tab if it's not already open
-    vim.cmd('edit ' .. selected_file.value)
+    vim.cmd('edit ' .. file_to_open)
+    if line_nr then
+        vim.api.nvim_win_set_cursor(0, {line_nr, 0})
+    end
 end
 
 require("telescope").setup({
