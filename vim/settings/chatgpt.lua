8ac00f9ba4a200d7f5cb76160701e1b5980a3e4e
Leon Li <qianthinking@gmail.com>
2024-05-21 14:34:04 +0800

    refactor: update chatgpt model version

--- vim/settings/chatgpt.lua
+++ vim/settings/chatgpt.lua
@@ -1,7 +1,7 @@
 require("chatgpt").setup({
   actions_paths = { "~/.vim/chatgpt/actions.json" },
   openai_params = {
-    model = "gpt-4-1106-preview",
+    model = "gpt-4o",
     frequency_penalty = 0,
     presence_penalty = 0,
     max_tokens = 1000,
@@ -10,7 +10,7 @@ require("chatgpt").setup({
     n = 1,
   },
   openai_edit_params = {
-    model = "gpt-4-1106-preview",
+    model = "gpt-4o",
     max_tokens = 1000,
     frequency_penalty = 0,
     presence_penalty = 0,
