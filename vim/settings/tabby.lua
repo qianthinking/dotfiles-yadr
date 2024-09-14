423821d151ab9be7c65666c59fd4e7302c00f3c3
Leon Li <qianthinking@gmail.com>
2024-09-14 14:30:47 +0800

    feat: tab style

--- vim/settings/tabby.lua
+++ vim/settings/tabby.lua
@@ -1,19 +1,77 @@
+local api = require('tabby.module.api')
+local buf_name = require('tabby.feature.buf_name')
+
+-- Retrieve the TabLineSel background dynamically
+local tabline_sel_bg = vim.api.nvim_get_hl_by_name('TabLineSel', true).background
+
+-- Define custom highlight groups for modified symbol and current window
+vim.api.nvim_set_hl(0, 'TabbyModified', { fg = '#dd5f5f', bold = true, bg = tabline_sel_bg })  -- Color for modified buffers
+-- Define the TabbyCurrentWin highlight group using the retrieved background
+vim.api.nvim_set_hl(0, 'TabbyCurrentWin', { fg = '#ffffff', bold = true, bg = tabline_sel_bg })
+vim.api.nvim_set_hl(0, 'TabbyCurrentWinSep', { fg = '#ffff00', bold = true, bg = tabline_sel_bg })
+
+local theme = {
+  fill = 'TabLineFill',
+  head = 'TabLine',
+  current_tab = 'TabLineSel',
+  tab = 'TabLine',
+  win = 'TabLine',
+  tail = 'TabLine',
+}
+
 require('tabby').setup({
   preset = 'tab_only',
   option = {
-    theme = {
-      fill = 'TabLineFill',       -- tabline background
-      head = 'TabLine',           -- head element highlight
-      current_tab = 'TabLineSel', -- current tab label highlight
-      tab = 'TabLine',            -- other tab label highlight
-      win = 'TabLine',            -- window highlight
-      tail = 'TabLine',           -- tail element highlight
-    },
-    nerdfont = true,              -- whether use nerdfont
-    lualine_theme = 'nordfox',          -- lualine theme name
+    theme = theme,
+    nerdfont = true,              -- whether to use nerdfont
+    lualine_theme = 'nordfox',    -- lualine theme name
     buf_name = {
       mode = "shorten",
     },
+    tab_name = {
+      name_fallback = function(tabid)
+        local wins = api.get_tab_wins(tabid)
+        local cur_win = api.get_tab_current_win(tabid)  -- Get the current window in the tab
+        local current_tab = api.get_current_tab()  -- Get the current active tab
+        local tab_name = ''
+        local valid_win_count = 0  -- Track windows that aren't 'NvimTree'
+
+        -- Iterate over each window in the tab
+        for i, win in ipairs(wins) do
+          local bufid = api.get_win_buf(win)
+          local name = buf_name.get(win)  -- Get the buffer name
+
+          -- Skip windows with 'NvimTree' in the name
+          if name:find('NvimTree') then
+            goto continue
+          end
+
+          -- Add indicator if buffer is modified (unsaved changes)
+          if api.get_buf_is_changed(bufid) then
+            name = name .. '%#TabbyModified#*%#TabLineSel#'  -- Append * with highlight for modified buffers
+          end
+
+          -- Highlight the current window only if this tab is active
+          if tabid == current_tab and win == cur_win then
+            name = '%#TabbyCurrentWin#' .. name .. '%#TabLineSel#'  -- Highlight the current window (fg only)
+          end
+
+          -- Append window name to the tab name
+          if valid_win_count > 0 then
+            tab_name = tab_name .. ' | '  -- Use '|' as a separator for valid windows
+          end
+          tab_name = tab_name .. name
+          valid_win_count = valid_win_count + 1
+
+          ::continue::
+        end
+
+        -- Return the concatenated name with all window names and modified indicators
+        return tab_name
+      end,
+    }
   },
 })
-vim.api.nvim_set_keymap("n", "<leader>gn", "<Cmd>Tabby jump_to_tab<CR>", {noremap = true, silent = true})
+
+-- Keymap for jumping to tabs
+vim.api.nvim_set_keymap("n", "<leader>gn", "<Cmd>Tabby jump_to_tab<CR>", { noremap = true, silent = true })
