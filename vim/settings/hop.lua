b04566ed5055c4e73045bd6bd655fd9e6551f910
Leon Li <qianthinking@gmail.com>
2024-11-03 17:42:34 +0800

    chore: adjust hop.lua key mappings
    Added key mappings for hopping to words before and after the cursor using Ctrl+K and Ctrl+J respectively. Also updated key mapping for hopping to words on the current line only. Removed key mapping for hopping to the second character of a word.

--- vim/settings/hop.lua
+++ vim/settings/hop.lua
@@ -4,11 +4,15 @@ require'hop'.setup({
 })
 local hop = require('hop')
 local hoptreesitter = require('hop-treesitter')
-vim.keymap.set({ "n", "x", "o" }, '<leader><leader>', function() hop.hint_words() end, {remap=true})
+local directions = require('hop.hint').HintDirection
+vim.keymap.set({ "n", "x", "o" }, '<C-K>', function() hop.hint_words({
+  direction = directions.BEFORE_CURSOR
+}) end, {remap=true})
+vim.keymap.set({ "n", "x", "o" }, '<C-J>', function() hop.hint_words({
+  direction = directions.AFTER_CURSOR
+}) end, {remap=true})
 vim.keymap.set({ "n", "x", "o" }, ';;', function() hop.hint_words({
   current_line_only = true,
-}) end, {remap=true})
-vim.keymap.set({ "n", "x", "o" }, 's', function() hop.hint_char2({
 }) end, {remap=true})
 
 
