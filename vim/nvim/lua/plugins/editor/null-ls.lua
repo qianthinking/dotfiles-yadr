8a72f40238b18ca34ee970dc5d9faacc561b6d9e
Leon Li <qianthinking@gmail.com>
2024-12-14 22:26:20 +0800

    chore: comment out on_attach function in null-ls.lua

--- vim/nvim/lua/plugins/editor/null-ls.lua
+++ vim/nvim/lua/plugins/editor/null-ls.lua
@@ -35,16 +35,16 @@ return {
             end,
           }),
         },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-        end,
+        --[[ on_attach = function(client, bufnr) ]]
+        --[[   if client.supports_method("textDocument/formatting") then ]]
+        --[[     vim.api.nvim_create_autocmd("BufWritePre", { ]]
+        --[[       buffer = bufnr, ]]
+        --[[       callback = function() ]]
+        --[[         vim.lsp.buf.format({ bufnr = bufnr }) ]]
+        --[[       end, ]]
+        --[[     }) ]]
+        --[[   end ]]
+        --[[ end, ]]
       })
     end,
   },
